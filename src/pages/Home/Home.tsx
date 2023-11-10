0efe2457679d52ef44cd7642671a530a77f79392
DenTex0 <denis200358@gmail.com>
2023-11-10 19:46:59 +0200

    routing

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { System as SystemLayout } from "../../layouts";
 import { useTranslation } from "react-i18next";
 import "leaflet/dist/leaflet.css";
@@ -6,9 +6,13 @@ import { mock } from "./mock";
 import { Col, Divider, Row } from "antd";
 import { constants } from "../../styles/constants";
 import { MapItem } from "./components/MapItem";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import Title from "antd/es/typography/Title";
 import { RestOutlined } from "@ant-design/icons";
+import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
+import "leaflet-routing-machine";
+
+import L from "leaflet";
 
 interface IProps {}
 
@@ -46,15 +50,22 @@ export const Home: FC<IProps> = (): JSX.Element => {
         </Col>
         <Col span={18} tabIndex="map">
           <div style={{ height: "100vh", overflowY: "auto", background: constants.light }}>
-            <MapContainer center={[50.45, 30.52]} maxBounds={[[50.156534, 31.138470], [50.959162, 29.471295]]} minZoom={12} maxZoom={18} zoom={16}>
+            <MapContainer
+              center={[ 50.45, 30.52 ]}
+              maxBounds={[ [ 50.156534, 31.138470 ], [ 50.959162, 29.471295 ] ]}
+              minZoom={12}
+              maxZoom={18}
+              zoom={16}
+            >
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
-              <Marker position={[50.45, 30.52]}>
+              <Marker position={[ 50.45, 30.52 ]}>
                 <Popup>
                   A pretty CSS3 popup. <br /> Easily customizable.
                 </Popup>
+                <Routing />
               </Marker>
             </MapContainer>
           </div>
@@ -62,4 +73,27 @@ export const Home: FC<IProps> = (): JSX.Element => {
       </Row>
     </SystemLayout>
   );
+};
+
+L.Marker.prototype.options.icon = L.icon({
+  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png"
+});
+
+const Routing = () => {
+  const map = useMap();
+
+  useEffect(() => {
+    if (!map) {
+      return;
+    }
+
+    const routingControl = L.Routing.control({
+      waypoints: [ L.latLng(50.452044, 30.515650), L.latLng(50.467651, 30.609239) ],
+      routeWhileDragging: true,
+    }).addTo(map);
+
+    return () => map.removeControl(routingControl);
+  }, [ map ]);
+
+  return null;
 };
